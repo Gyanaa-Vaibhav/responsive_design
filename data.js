52a5d3cc01ca563563bd9ae68773cfc0f212b55c
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-10-13 20:24:13 +0530

    failed progress

---
+++ data.js
@@ -0,0 +1,41 @@
+const Journals = [
+  {
+    id: 1,
+    date: "July 23,2023",
+    title: "My new journey as a bootcamp student.",
+    image: "./images/featured.png",
+    description:
+      "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+    isFeatured: true,
+  },
+
+  {
+    id: 2,
+    date: "July 23,2023",
+    title: "Blog One",
+    image: "images/blog_1.png",
+    description:
+      "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+    isFeatured: false,
+  },
+
+  {
+    id: 3,
+    date: "July 23,2023",
+    title: "My new journey as a bootcamp student.",
+    image: "images/blog_2.png",
+    description:
+      "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+    isFeatured: false,
+  },
+
+  {
+    id: 4,
+    date: "July 23,2023",
+    title: "My new journey as a bootcamp student.",
+    image: "images/blog_3.png",
+    description:
+      "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+    isFeatured: false,
+  },
+];
